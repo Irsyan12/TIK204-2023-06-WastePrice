fea604071dd65decec611efd4bb84e8f971b5095
ade <ade_s21@mhs.unsyiah.ac.id>
2023-05-08 11:28:50 +0700

    harga sampah

--- hargasampah.php
+++ hargasampah.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
 include 'homeheader.php';
 include 'auth/koneksi.php';
+if (!isset($_SESSION['session_username'])) {
+    header('location:login');
+    exit();
+}
 // Select data from table
 $sql = "SELECT kategori_sampah, jenis_sampah, keterangan, harga_sampah FROM tb_sampah";
 $result = mysqli_query($conn, $sql);
@@ -11,15 +16,11 @@ $result = mysqli_query($conn, $sql);
 
 <head>
     <title>Harga Sampah</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Load Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="container mt-3">
-        <h1 class="mb-3">Harga Sampah</h1>
+    <div class="container mt-5 mb-5">
+        <h1 class="mb-3 mt-5 my-5 pt-4">Harga Sampah</h1>
         <div class="align-items-center">
             <form id="searchTask">
                 <div class="col-auto mb-3">
@@ -28,7 +29,7 @@ $result = mysqli_query($conn, $sql);
                 </div>
             </form>
         </div>
-        <table class="table table-striped">
+        <table class="table table-striped mb-5 pb-5">
             <thead>
                 <tr>
                     <th>Kategori Sampah</th>
@@ -49,27 +50,32 @@ $result = mysqli_query($conn, $sql);
                         echo "<td class='item_sampah kategori'>" . $row["kategori_sampah"] . "</td>";
                         echo "<td class='item_sampah jenis'>" . $row["jenis_sampah"] . "</td>";
                         echo "<td class='item_sampah keterangan'>" . $row["keterangan"] . "</td>";
-                        echo "<td class='harga'>" . $row["harga_sampah"] . "</td>";
+                        echo "<td class='harga'>" . number_format($row["harga_sampah"], 0, ',', '.') . "</td>";
                         echo "</tr>";
                     }
-                } else {
-                    echo "<tr><td colspan='4'>Tidak ada data yang ditemukan</td></tr>";
                 }
 
                 // Close database connection
                 mysqli_close($conn);
                 ?>
+                <tr>
+                    <td colspan="4" id="noDataMessage" class="text-center">Sampah tidak ditemukan</td>
+                </tr>
             </tbody>
         </table>
     </div>
+
+    <?php
+    include 'navbar.php';
+    ?>
     <!-- Load Bootstrap JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js"></script>
     <script>
         const searchTask = document.getElementById('inputSearch');
         searchTask.addEventListener('keyup', function (e) {
+
             const searchString = e.target.value.toLowerCase();
             const tableRows = document.querySelectorAll('.table tbody tr');
-
             tableRows.forEach((row) => {
                 const kategori = row.querySelector('.kategori').textContent.toLowerCase();
                 const jenis = row.querySelector('.jenis').textContent.toLowerCase();
@@ -78,23 +84,18 @@ $result = mysqli_query($conn, $sql);
 
                 if (kategori.indexOf(searchString) !== -1 || jenis.indexOf(searchString) !== -1 || keterangan.indexOf(searchString) !== -1 || harga.indexOf(searchString) !== -1) {
                     row.classList.remove('d-none');
-                    foundRows = true;
+                    // foundRows = true;
                 } else {
                     row.classList.add('d-none');
                 }
             });
 
-            const noDataMessage = document.getElementById('noDataMessage');
-            if (foundRows) {
-                noDataMessage.classList.add('d-none');
-            } else {
-                noDataMessage.classList.remove('d-none');
-            }
+
         });
 
 
     </script>
-    <script src="script.js"></script>
+    <script src="js/script.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.min.js"></script>
 </body>
 
