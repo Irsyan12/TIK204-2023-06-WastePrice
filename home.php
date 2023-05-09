2c11f39e2886063820da1c6a07b1126dd968192b
irsan72 <irsyah@mhs.unsyiah.ac.id>
2023-05-09 15:34:57 +0700

    alert jual

--- home.php
+++ home.php
@@ -93,6 +93,7 @@ if (!isset($_SESSION['session_username'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
         crossorigin="anonymous"></script>
+    <script src="alert/sweetalert2.all.min.js"></script>
     <script src="js/alert.js"></script>
     <script>
         <?php if (isset($_SESSION['jualBerhasil'])):
