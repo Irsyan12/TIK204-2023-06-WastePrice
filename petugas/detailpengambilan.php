5aaf381cb9ea6e57208529ae5c1e94290e818e4d
nadia <n_turrahmi@mhs.unsyiah.ac.id>
2023-05-11 12:09:35 +0700

    detail pengambilan

---
+++ petugas/detailpengambilan.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Pengambilan</title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css">
+</head>
+<body>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <div class="card">
+          <div class="card-header">
+            Detail Pengambilan
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">Riwayat Pengambilan</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus fringilla purus, at cursus est finibus ac. Duis varius massa non ligula finibus, id semper neque tincidunt.</p>
+            <h5 class="card-title">Alamat Penjemputan</h5>
+            <p class="card-text">Jl. Contoh Alamat No. 123, Kota Contoh, Negara Contoh</p>
+            <h5 class="card-title">Deskripsi Sampah</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus fringilla purus, at cursus est finibus ac. Duis varius massa non ligula finibus, id semper neque tincidunt.</p>
+            <h5 class="card-title">Waktu Pengambilan</h5>
+            <p class="card-text">10 Mei 2023, Pukul 08.00 WIB</p>
+            <a href="#" class="btn btn-primary">Edit</a>
+            <a href="#" class="btn btn-success">Ambil Sekarang</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap JS -->
